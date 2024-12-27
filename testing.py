543d5686c2691cef8ce662efdd69976723a9f795
unknown <yashaswinibhushan22@gmail.com>
2024-12-27 22:38:33 +0530

    addition

--- testing.py
+++ testing.py
@@ -1,10 +1,4 @@
 
 from main import add
 
-def test_add():
-    assert add(2,3) == 5
-    print("Assertion passed!")
-    assert add(0, 42) == 42
-    print("Assertion passed!")
-    assert add(-1, 1) == 0
-    print("Assertion passed!")
+print("Result:",add(2,3))
